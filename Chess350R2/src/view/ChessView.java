74f65dfbbb4b54e1736233aba8f1da96ad8f614b
rosenberga <rosenbea@mail.gvsu.edu>
2014-03-22 23:22:36 -0400

    checkstyle should be done

--- Chess350R2/src/view/ChessView.java
+++ Chess350R2/src/view/ChessView.java
@@ -4,146 +4,225 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Toolkit;
-import java.awt.event.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JMenuBar;
+import javax.swing.JPanel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
-import javax.swing.*;
-
+/*****************************************************************
+ * The view class.
+ * 
+ * @author Adam Rosenberg
+ * @version 1.0
+ *****************************************************************/
 public class ChessView implements IChessView {
+	
+	/** Number of colors in the game. */
 	private static final int NUM_COLORS = 2;
-	private static final int NUM_PIECES = 7;
-	private static final int OWNER = 0;
-	private static final int TYPE = 1;
-	private static final int PIECE_INFO = 2;
-	private static final int ROW = 0;
-	private static final int COL = 1;
-	private static final String BLACK_SPACE = "blackSpace.png";
-	private static final String WHITE_SPACE = "whiteSpace.png";
-	private static final String LEGAL = "legal.png";
-	private static final String SELECTED = "selected.png";
-	private static final String EMPTY = "piece-1-1.png";
-	private static final double RATIO = .83;
-	
-	private int cols;
-	private int rows;
-	
-	private JPanel panel;
-	private JPanel piecePanel;
-	private JFrame frame;
-	private ImageIcon[][] pieceImages;
-	private JButton[][] pieceLabels;
-	
-	/** The Menu Bar */
-    private JMenuBar menus;
-    
-   /** Game Option for Menu Bar */
-    private JMenu gameMenu;
-    
-   /** Help Option for Menu Bar */
-    private JMenu helpMenu;
-    
-   /** Settings Option for Menu Bar */
-    private JMenu settingsMenu;
-    
-   /** New Game Menu Item */
-    private JMenuItem newGameItem;
-    
-   /** Exit Menu Item */
-    private JMenuItem exitItem; 
-    
-   /** About Menu Item */
-    private JMenuItem aboutItem;
-    
-    /** Setting Menu Item */
-    private JMenuItem settingItem;
 
-	private boolean showLegal;
+	/** Number of different types of pieces. */
+	private static final int NUM_PIECES = 7;
 	
-	public ChessView(final int r, final int c) {
+	/** Owner position in piece info int[]. */
+	private static final int OWNER = 0;
+	
+	/** Piece type position in piece info int[]. */
+	private static final int TYPE = 1;
+	
+	/** Size of int[] with piece info. */
+	private static final int PIECE_INFO = 2;
+	
+	/** Represents the row in a JButton array. */
+	private static final int ROW = 0;
+	
+	/** Represents the column in a JButton array. */
+	private static final int COL = 1;
+	
+	/** Picture of black space. */
+	private static final String BLACK_SPACE = "blackSpace.png";
+	/** Picture of white space. */
+	private static final String WHITE_SPACE = "whiteSpace.png";
+	/** Picture of legal space. */
+	private static final String LEGAL = "legal.png";
+	/** Picture of selected space. */
+	private static final String SELECTED = "selected.png";
+	/** Picture of empty space. */
+	private static final String EMPTY = "piece-1-1.png";
+	/** Ratio to use when sizing the frame. */
+	private static final double RATIO = 0.83;
+
+	/** Number of columns. */
+	private int cols;
+	/** number of rows. */
+	private int rows;
+
+	/** Panel to put the board on. */
+	private JPanel panel;
+	/** Panel to put images on. */
+	private JPanel piecePanel;
+	/** Frame to put panel in. */
+	private JFrame frame;
+	/** Array of images. */
+	private ImageIcon[][] pieceImages;
+	/** Array of chess-square buttons. */
+	private JButton[][] pieceLabels;
+
+	/** The Menu Bar. */
+	private JMenuBar menus;
+
+	/** Game Option for Menu Bar. */
+	private JMenu gameMenu;
+
+	/** Help Option for Menu Bar. */
+	private JMenu helpMenu;
+
+	/** Settings Option for Menu Bar. */
+	private JMenu settingsMenu;
+
+	/** New Game Menu Item. */
+	private JMenuItem newGameItem;
+
+	/** Exit Menu Item. */
+	private JMenuItem exitItem; 
+
+	/** About Menu Item. */
+	private JMenuItem aboutItem;
+
+	/** Setting Menu Item. */
+	private JMenuItem settingItem;
+
+	/** Shows if the square is a legal move. */
+	private boolean showLegal;
+
+	/*****************************************************************
+	 * Constructor for the View.
+	 * 
+	 * @param row number of rows
+	 * @param col number of columns
+	 *****************************************************************/
+	public ChessView(final int row, final int col) {
 		super();
-		this.rows = r;
-		this.cols = c;
+		this.rows = row;
+		this.cols = col;
 		frame = new JFrame();
 		newGame();
 		showLegal = true;
 	}
-	   public boolean isShowLegal() {
-			return showLegal;
-		}
+	/*****************************************************************
+	 * Returns true if the square is a legal move.
+	 * 
+	 * @return showlegal 
+	 *****************************************************************/
+	public final boolean isShowLegal() {
+		return showLegal;
+	}
 
-		public void setShowLegal(boolean sL) {
-			this.showLegal = sL;
-		}
+	/*****************************************************************
+	 * Sets the legality of a move.
+	 * 
+	 * @param showLegal1 the legality
+	 *****************************************************************/
+	public final void setShowLegal(final boolean showLegal1) {
+		this.showLegal = showLegal1;
+	}
+	
+	/*****************************************************************
+	 * Create a new game.
+	 * 
+	 *****************************************************************/
 	private void newGame() {
 		frame.dispose();
 		setUpBase();
 	}
-	
+
+	/*****************************************************************
+	 * Builds the GUI.
+	 * 
+	 *****************************************************************/
 	private void setUpBase() {
 		frame = new JFrame("Chess350");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		setUpMenu();
 		setUpPanels();
 		frame.getContentPane().add(panel);
 		frame.setJMenuBar(menus);
-		
+
 		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
 		double height = screenSize.getHeight();
 		int h = (int) (height * RATIO);
-		
+
 		frame.setPreferredSize(new Dimension(h, h));
 		frame.pack();
-		frame.setResizable(false);
 		frame.setVisible(true);
 	}
-	
+
+	/*****************************************************************
+	 * Builds the Menu Bar.
+	 *****************************************************************/
 	private void setUpMenu() {
-		
+
 		menus = new JMenuBar();
-		
-	    gameMenu = new JMenu("Game");
-	    settingsMenu = new JMenu("Settings");
-	    helpMenu = new JMenu("Help");
-	    
-	    newGameItem = new JMenuItem("New Game");
-        exitItem = new JMenuItem("Exit"); 
-        aboutItem = new JMenuItem("About");
-	    settingItem = new JMenuItem("Game Settings");
-	    
-	    gameMenu.add(newGameItem);
-	    gameMenu.add(exitItem);
-	    settingsMenu.add(settingItem);
-	    helpMenu.add(aboutItem);
-	    
-	    menus.add(gameMenu);
-	    menus.add(settingsMenu);
-	    menus.add(helpMenu);
-		
+
+		gameMenu = new JMenu("Game");
+		settingsMenu = new JMenu("Settings");
+		helpMenu = new JMenu("Help");
+
+		newGameItem = new JMenuItem("New Game");
+		exitItem = new JMenuItem("Exit"); 
+		aboutItem = new JMenuItem("About");
+		settingItem = new JMenuItem("Game Settings");
+
+		gameMenu.add(newGameItem);
+		gameMenu.add(exitItem);
+		settingsMenu.add(settingItem);
+		helpMenu.add(aboutItem);
+
+		menus.add(gameMenu);
+		menus.add(settingsMenu);
+		menus.add(helpMenu);
+
 	}
-	
+
+	/*****************************************************************
+	 * Builds the panels.
+	 *****************************************************************/
 	private void setUpPanels() {
 		panel = new JPanel();
 		panel.setLayout(new BorderLayout());
-		
+
 		piecePanel = new JPanel();
 		setUpPiecePanel();
 		panel.add(piecePanel, BorderLayout.CENTER);
 	}
-	
+
+	/*****************************************************************
+	 * Sets the legality of a move.
+	 *****************************************************************/
 	private void setUpPiecePanel() {
 		piecePanel.setLayout(new GridLayout(rows, cols));
 		setUpImages();
 	}
-	
+
+	/*****************************************************************
+	 * Sets up the images.
+	 *****************************************************************/
 	private void setUpImages() {
 		pieceImages = new ImageIcon[NUM_COLORS][NUM_PIECES];
 		for (int i = 0; i < pieceImages.length; i++) {
 			for (int j = 0; j < pieceImages[i].length; j++) {
-				pieceImages[i][j] = new ImageIcon("piece" + i + "" + j 
-						+ ".png", "piece" + i + "" + j + ".png");
+				pieceImages[i][j] = new ImageIcon("piece" 
+						+ i + "" + j + ".png", 
+							"piece" + i + "" + j + ".png");
 			}
 		}
-		
+
 		pieceLabels = new JButton[rows][cols];
 		for (int i = 0; i < pieceLabels.length; i++) {
 			for (int j = 0; j < pieceLabels[i].length; j++) {
@@ -151,23 +230,34 @@ public class ChessView implements IChessView {
 				piecePanel.add(pieceLabels[i][j]);
 			}
 		}
-		
+
 	}
 
+	/*****************************************************************
+	 * Adds an action listener to all clickable components.
+	 * 
+	 * @param e the action listener to add
+	 *****************************************************************/
 	@Override
-	public void addButtonListeners(final ActionListener e) {
+	public final void addButtonListeners(final ActionListener e) {
 		for (int i = 0; i < rows; i++) {
 			for (int j = 0; j < cols; j++) {
 				pieceLabels[i][j].addActionListener(e);
 			}
 		}
-		
-	    newGameItem.addActionListener(e);
-        exitItem.addActionListener(e);
-        aboutItem.addActionListener(e);
-	    settingItem.addActionListener(e);
+
+		newGameItem.addActionListener(e);
+		exitItem.addActionListener(e);
+		aboutItem.addActionListener(e);
+		settingItem.addActionListener(e);
 	}
 
+	/*****************************************************************
+	 * Returns an int[] containing information about a chess location.
+	 * 
+	 * @param e the actionevent that was created by a click event
+	 * @return an int[] containing information about a chess location
+	 *****************************************************************/
 	@Override
 	public final int[] getData(final ActionEvent e) {
 		int[] pos = new int[PIECE_INFO];
@@ -183,16 +273,30 @@ public class ChessView implements IChessView {
 		return pos;
 	}
 
+	/*****************************************************************
+	 * Sets a new icon for a given location.
+	 * 
+	 * @param row the row to change
+	 * @param col the column to change
+	 * @param pieceID an int array with info on the what to change
+	 * 			the location to
+	 *****************************************************************/
 	@Override
-	public final void setData(final int row, final int col, final int[] pieceID) {
+	public final void setData(final int row, final int col, 
+			final int[] pieceID) {
 		ImageIcon icon;
 		try {
-		icon = pieceImages[pieceID[OWNER]][pieceID[TYPE]];
+			icon = pieceImages[pieceID[OWNER]][pieceID[TYPE]];
 		} catch (Exception e) {
+			
+			// then one of ints in the array was -1
+			// thus set the icon to show an empty space
 			icon = new ImageIcon(EMPTY, EMPTY);
 		}
-		if ((row % 2 == 0 && col % 2 == 0)
-				|| (row % 2 != 0 && col % 2 != 0)) {
+		
+		// else it was a piece, so determine if it should be a white or
+		// black background
+		if ((row % 2 == 0 && col % 2 == 0) || (row % 2 != 0 && col % 2 != 0)) {
 			icon = new CustomIcon(WHITE_SPACE, icon);
 		} else {
 			icon = new CustomIcon(BLACK_SPACE, icon);
@@ -204,11 +308,21 @@ public class ChessView implements IChessView {
 	public final void showMessage(final String m) {
 		JOptionPane.showMessageDialog(frame, m);
 	}
-	
-	public void paintLegalMove(int row, int col, int[] pieceID) {
+
+	/*****************************************************************
+	 * Sets the legality of a move.
+	 * 
+	 * @param row paint at this row
+	 * @param col paint at this column
+	 * @param pieceID array of places to paint
+	 *****************************************************************/
+	public final void paintLegalMove(final int row, final int col, 
+			final int[] pieceID) {
+		
+		// shows legal moves with a green background
 		ImageIcon icon;
 		try {
-		icon = pieceImages[pieceID[OWNER]][pieceID[TYPE]];
+			icon = pieceImages[pieceID[OWNER]][pieceID[TYPE]];
 		} catch (Exception e) {
 			icon = new ImageIcon(EMPTY, EMPTY);
 		}
@@ -216,37 +330,67 @@ public class ChessView implements IChessView {
 		pieceLabels[row][col].setIcon(icon);
 	}
 
-	public void showSelected(int row, int col, int[] pieceID) {
+	/*****************************************************************
+	 * Show the legality of a move.
+	 * 
+	 * @param row paint at this row
+	 * @param col paint at this column
+	 * @param pieceID array of places to paint
+	 *****************************************************************/
+	public final void showSelected(final int row, 
+			final int col, final int[] pieceID) {
+		
+		// set the JButton to have a blue background
 		ImageIcon icon = pieceImages[pieceID[OWNER]][pieceID[TYPE]];
 		icon = new CustomIcon(SELECTED, icon);
 		pieceLabels[row][col].setIcon(icon);
 	}
 
-	@Override
-	public final JMenu getSettingsMenu() {
-		return settingsMenu;
-	}
-
+	/*****************************************************************
+	 * Returns the exits menu item.
+	 * 
+	 * @return a jmenuitem that closes the program
+	 *****************************************************************/
 	@Override
 	public final JMenuItem getExitItem() {
 		return exitItem;
 	}
 
+	/*****************************************************************
+	 * Returns the about menu item.
+	 * 
+	 * @return a jmenuitem that opens an about dialog
+	 *****************************************************************/
 	@Override
 	public final JMenuItem getAboutItem() {
 		return aboutItem;
 	}
 
+	/*****************************************************************
+	 * Returns the settings menu item.
+	 * 
+	 * @return a jmenuitem that opens a setting dialog
+	 *****************************************************************/
 	@Override
 	public final JMenuItem getSettingItem() {
 		return settingItem;
 	}
 
+	/*****************************************************************
+	 * Returns the new game menu item.
+	 * 
+	 * @return a jmenuitem that starts a new game
+	 *****************************************************************/
 	@Override
 	public final JMenuItem getNewGameItem() {
 		return newGameItem;
 	}
 
+	/*****************************************************************
+	 * Removes an action listener from all JButtons.
+	 * 
+	 * @param e the action listener to remove
+	 *****************************************************************/
 	@Override
 	public final void disable(final ActionListener e) {
 		for (int i = 0; i < pieceLabels.length; i++) {
@@ -255,12 +399,20 @@ public class ChessView implements IChessView {
 			}
 		}
 	}
-	
-	public void close() {
+
+	/*****************************************************************
+	 * Get rid of the frame.
+	 *****************************************************************/
+	public final void close() {
 		frame.setVisible(false);
 		frame.dispose();
 	}
 
+	/*****************************************************************
+	 * Sets the legality of a move.
+	 * 
+	 * @return frame the user window
+	 *****************************************************************/
 	@Override
 	public final JFrame getFrame() {
 		return frame;
--- Chess350R2/src/view/IChessView.java
+++ Chess350R2/src/view/IChessView.java
@@ -1,28 +1,125 @@
 package view;
 
-import java.awt.event.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 import javax.swing.JFrame;
-import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
+/*****************************************************************
+ * The Interface for the view class.
+ * 
+ * @author Adam Rosenberg
+ * @version 1.0
+ *****************************************************************/
 public interface IChessView {
 
-		void close();
-		void addButtonListeners(ActionListener e);
-		int[] getData(ActionEvent e); // gives four coordinates of move
-		void setData(int row, int col, int[] pieceID); // return board
-		void showMessage(String m); // error message?
-		void paintLegalMove(int row, int col, int[] pieceID);
-		JMenu getSettingsMenu();
-		JMenuItem getAboutItem();
-		JMenuItem getSettingItem();
-	        JMenuItem getNewGameItem();
-	        JMenuItem getExitItem();
-			void showSelected(int i, int j, int[] sendPiece);
-			void disable(ActionListener actionListener);
-	   void setShowLegal(boolean b);
-	   boolean isShowLegal();
+	/** Close the GUI. */
+	void close();
+	
+	/***************************************************************** 
+	 * Add listeners to materials and implement actionPerformed. 
+	 * 
+	 * @param e the actionlistener variable
+	  *****************************************************************/
+	void addButtonListeners(ActionListener e);
+	
+	/*****************************************************************
+	 * Retrieve coordinate data from the board. 
+	 * 
+	 * @param e the actionevent variable
+	 * @return int[]
+	  *****************************************************************/
+	int[] getData(ActionEvent e); // gives four coordinates of move
+	
+	/*****************************************************************
+	 *  Set the coordinates into the board. 
+	 *
+	 * @param row the new row
+	 * @param col the new column
+	 * @param pieceID all the pieces
+	  *****************************************************************/
+	void setData(int row, int col, int[] pieceID);
+	
+	/*****************************************************************
+	 * Show the message of the game. 
+	 * 
+	 * @param m the message
+	 *****************************************************************/
+	void showMessage(String m);
+	
+	/*****************************************************************
+	 * Mark the squares to show what is legal. 
+	 * 
+	 * @param row the new row
+	 * @param col the new column
+	 * @param pieceID all the pieces
+	 *****************************************************************/
+	void paintLegalMove(int row, int col, int[] pieceID);
+	
+	/*****************************************************************
+	 * Returns the JMenuItem About field of the view.
+	 * 
+	 * @return JMenuItem
+	 *****************************************************************/
+	JMenuItem getAboutItem();
+	
+	/*****************************************************************
+	 * Returns the JMenuItem Settings field of the view.
+	 * 
+	 * @return JMenuItem
+	 *****************************************************************/
+	JMenuItem getSettingItem();
+	
+	/*****************************************************************
+	 * Returns the JMenuItem New Game field of the view.
+	 * 
+	 * @return JMenuItem
+	 *****************************************************************/
+	JMenuItem getNewGameItem();
+	
+	/*****************************************************************
+	 * Returns the JMenuItem Exit field of the view.
+	 * 
+	 * @return JMenuItem
+	 *****************************************************************/
+	JMenuItem getExitItem();
+	
+	/*****************************************************************
+	 * Shows selected piece's square.
+	 * 
+	 * @param i coordinate x
+	 * @param j coordinate y
+	 * @param sendPiece the array of pieces to send
+	 *****************************************************************/
+	void showSelected(int i, int j, int[] sendPiece);
+	
+	/*****************************************************************
+	 * Disables the board.
+	 * 
+	 * @param actionListener disables all squares
+	 *****************************************************************/
+	void disable(ActionListener actionListener);
+	
+	/*****************************************************************
+	 * The squares will show legal moves if true.
+	 * 
+	 * @param b used to set legality
+	 *****************************************************************/
+	void setShowLegal(boolean b);
+	
+	/*****************************************************************
+	 * Returns the legality of the move to the sqaure.
+	 * 
+	 * @return boolean
+	 *****************************************************************/
+	boolean isShowLegal();
+	
+	/*****************************************************************
+	 * Returns the JFrame field of the view.
+	 * 
+	 * @return JFrame
+	 *****************************************************************/
 	JFrame getFrame();
 
 }
