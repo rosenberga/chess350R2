dedce2a327edd15a6e63efe7b66e129ccaac0b45
sagegithub <heisss@mail.gvsu.edu>
2014-04-18 01:48:03 -0400

    change to startscreen checkstyle

--- Chess350R2/src/view/StartScreen.java
+++ Chess350R2/src/view/StartScreen.java
@@ -36,12 +36,21 @@ public class StartScreen {
     private JButton styleThree;
     /** Loads last saved game. */
     private JButton loadGame;
+    /** Font size. */
     private static final int FONT_SIZE = 20;
+    /** Dimension 300. */
     private static final int DIM = 300;
+    /** Magic number c1. */
     private static final int C1 = 204;
+    /** Magic number c2. */
     private static final int C2 = 229;
+    /** Magic number c3. */
     private static final int C3 = 255;
 
+    /*****************************************************************
+     * Constructor for the class.
+     * 
+     *****************************************************************/
     public StartScreen() {
         frame = new JFrame("Chess350");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -71,13 +80,18 @@ public class StartScreen {
         frame.setLocation(dim.width / 2 - frame.getSize().width / 2, dim.height
                 / 2 - frame.getSize().height / 2);
         frame.setResizable(false);
-        frame.setSize(300, 300);
+        frame.setSize(DIM, DIM);
 
         changeButtonLook(loadGame);
         changeButtonLook(onePlayer);
         changeButtonLook(twoPlayer);
     }
 
+    /*****************************************************************
+     * Add an action listener to componen objects.
+     * 
+     * @param e the actionlister to add
+     *****************************************************************/
     public final void addActionListeners(final ActionListener e) {
         onePlayer.addActionListener(e);
         twoPlayer.addActionListener(e);
@@ -87,6 +101,10 @@ public class StartScreen {
         loadGame.addActionListener(e);
     }
 
+    /*****************************************************************
+     * Edits the start screen window.
+     * 
+     *****************************************************************/
     public final void changePanels() {
         frame.getContentPane().remove(firstPanel);
         frame.getContentPane().add(secondPanel);
@@ -98,35 +116,75 @@ public class StartScreen {
         frame.setResizable(false);
     }
 
-    public JButton getOnePlayer() {
+    /*****************************************************************
+     * Returns the button for having one player.
+     * 
+     * @return the one player button
+     *****************************************************************/
+    public final JButton getOnePlayer() {
         return onePlayer;
     }
 
-    public JButton getTwoPlayer() {
+    /*****************************************************************
+     * Returns the button for having two player.
+     * 
+     * @return the two player button
+     *****************************************************************/
+    public final JButton getTwoPlayer() {
         return twoPlayer;
     }
 
-    public JButton getStyleOne() {
+    /*****************************************************************
+     * Returns the button for having easy computer.
+     * 
+     * @return the easy conputer button
+     *****************************************************************/
+    public final JButton getStyleOne() {
         return styleOne;
     }
 
-    public JButton getStyleTwo() {
+    /*****************************************************************
+     * Returns the button for having medium difficulty computer.
+     * 
+     * @return the medium conputer button
+     *****************************************************************/
+    public final JButton getStyleTwo() {
         return styleTwo;
     }
 
-    public JButton getStyleThree() {
+    /*****************************************************************
+     * Returns the button for having hard computer.
+     * 
+     * @return the hard conputer button
+     *****************************************************************/
+    public final JButton getStyleThree() {
         return styleThree;
     }
 
-    public JButton getLoadGame() {
+    /*****************************************************************
+     * Returns the button for loading a saved game.
+     * 
+     * @return the loading button
+     *****************************************************************/
+    public final JButton getLoadGame() {
         return loadGame;
     }
 
-    public void setVisible(final boolean visable) {
-        frame.setVisible(visable);
+    /*****************************************************************
+     * Makes the window visible.
+     * 
+     * @param visible true if visible to user
+     *****************************************************************/
+    public final void setVisible(final boolean visible) {
+        frame.setVisible(visible);
     }
 
-    private void changeButtonLook(JButton b) {
+    /*****************************************************************
+     * Customize button attributes.
+     * 
+     * @param b the button that needs changing
+     *****************************************************************/
+    private void changeButtonLook(final JButton b) {
         b.setBackground(new Color(C1, C2, C3));
         b.setForeground(Color.black);
         b.setFocusPainted(false);
