ed99b4aec8332eb66900339d4c8ca85f089d8dc6
Chrisness <Chris@35.40.65.241>
2014-04-18 02:04:55 -0400

    cs

--- Chess350R2/src/model/King.java
+++ Chess350R2/src/model/King.java
@@ -2,16 +2,16 @@ package model;
 
 /*****************************************************************
  * A King in a game of chess.
- * 
+ *
  * @author Adam Rosenberg
  * @version 1.0
  *****************************************************************/
 public final class King extends ChessPiece {
 
-	
+
 	/*****************************************************************
 	 * Constructs a new King object.
-	 * 
+	 *
 	 * @param color
 	 *            the player that owns this piece
 	 *****************************************************************/
@@ -19,10 +19,10 @@ public final class King extends ChessPiece {
 		super(color);
 	}
 
-	
+
 	/*****************************************************************
 	 * Returns the type of ChessPiece it is.
-	 * 
+	 *
 	 * @return the String value of the name of the ChessPiece
 	 *****************************************************************/
 	@Override
@@ -30,10 +30,10 @@ public final class King extends ChessPiece {
 		return "King";
 	}
 
-	
+
 	/*****************************************************************
 	 * Checks if a move is valid for the chess piece.
-	 * 
+	 *
 	 * @param move
 	 *            the move to check for validity
 	 * @param board
@@ -43,7 +43,7 @@ public final class King extends ChessPiece {
 	 * @return true if the move is valid, else false
 	 *****************************************************************/
 	@Override
-	public boolean isValidMove(final Move move, 
+	public boolean isValidMove(final Move move,
 			final IChessBoard board, final IChessModel model) {
 		if (!super.isValidMove(move, board, model)) {
 			return false;
@@ -52,43 +52,46 @@ public final class King extends ChessPiece {
 		// King can only move one square at a time but he can move
 		// forward, backward, left, right, and diagonally.
 		if (Math.abs(move.getToColumn() - move.getFromColumn()) <= 1
-				&& Math.abs(move.getToRow() - move.getFromRow()) <= 1) {
+				&& Math.abs(move.getToRow()
+						- move.getFromRow()) <= 1) {
 			return true;
 		}
-		
+
 		// check for castle too
 		if (!getMoved()) {
-			
+
 			// check if the castle if to the right or is to the left
-			if ((Math.abs(move.getToColumn() 
-					- move.getFromColumn()) == 2) 
-						&& move.getFromRow() == move.getToRow()) {
+			if ((Math.abs(move.getToColumn()
+					- move.getFromColumn()) == 2)
+						&& move.getFromRow()
+						== move.getToRow()) {
 				IChessPiece piece;
 				if (move.getToColumn() > move.getFromColumn()) {
-					
+
 					// moving to the right
-					piece = board.pieceAt(move.getFromRow(), 
+					piece = board.pieceAt(move.getFromRow(),
 							board.numColumns() - 1);
 				} else {
-					
+
 					// else moving to the left
-					piece = board.pieceAt(move.getFromRow(), 0);
+					piece = board.pieceAt(move.
+							getFromRow(), 0);
 				}
-				
-				// check to make sure the rook is there 
+
+				// check to make sure the rook is there
 				// and its has the same owner
 				if (piece == null
 						|| !piece.type().equals("Rook")
-							|| piece.player() != this.player() 
+							|| piece.player() != this.player()
 								|| piece.getMoved()) {
 					return false;
 				} else {
-					
+
 					// make sure the king is not in check
 					if (model.inCheck(board)) {
 						return false;
 					}
-					return !movingThroughCheckOrAnotherPiece(
+				   return !movingThroughCheckOrAnotherPiece(
 							move, board, model);
 				}
 			}
@@ -96,11 +99,11 @@ public final class King extends ChessPiece {
 		return false;
 	}
 
-	
+
 	/*****************************************************************
 	 * Checks to see if the king is moving through check
 	 * or another piece when castling.
-	 * 
+	 *
 	 * @param move the move to be made
 	 * @param board the current IChessBoard
 	 * @param model the IChessModel to use when checking
@@ -109,11 +112,11 @@ public final class King extends ChessPiece {
 	 *****************************************************************/
 	private boolean movingThroughCheckOrAnotherPiece(final Move move,
 			final IChessBoard board, final IChessModel model) {
-		
+
 		// create a new board
 		IChessBoard fakeBoard = new ChessBoard();
 		fakeBoard.setBoard(board.copyBoard());
-		
+
 		// get the direction
 		int direction;
 		if (move.getFromColumn() < move.getToColumn()) {
@@ -121,12 +124,12 @@ public final class King extends ChessPiece {
 		} else {
 			direction = -1;
 		}
-		
+
 		// move through the fakeboard checking if the position is
 		// occupied or if the position would put the king in check
-		for (int i = move.getFromColumn() + direction; 
+		for (int i = move.getFromColumn() + direction;
 				i != move.getToColumn(); i += direction) {
-			if (model.pieceAt(fakeBoard, move.getFromRow(), i) 
+			if (model.pieceAt(fakeBoard, move.getFromRow(), i)
 					!= null) {
 				return true;
 			}
@@ -139,10 +142,10 @@ public final class King extends ChessPiece {
 		return false;
 	}
 
-	
+
 	/*****************************************************************
 	 * Set the current piece to reflect it being moved.
-	 * 
+	 *
 	 * @param moved
 	 *            a boolean stating if the piece was moved
 	 *****************************************************************/
@@ -151,11 +154,11 @@ public final class King extends ChessPiece {
 		super.setMoved(moved);
 	}
 
-	
+
 	/*****************************************************************
-	 * Returns a boolean that is true or false depending on if the 
+	 * Returns a boolean that is true or false depending on if the
 	 * piece has moved yet.
-	 * 
+	 *
 	 * @return a boolean stating if the piece has moved yet or not
 	 *****************************************************************/
 	@Override
--- Chess350R2/src/model/Knight.java
+++ Chess350R2/src/model/Knight.java
@@ -2,7 +2,7 @@ package model;
 
 /*****************************************************************
  * A Knight in a game of chess.
- * 
+ *
  * @author Adam Rosenberg
  * @version 1.0
  *****************************************************************/
@@ -10,18 +10,18 @@ public final class Knight extends ChessPiece {
 
 	/*****************************************************************
 	 * Constructs a new Knight object.
-	 * 
+	 *
 	 * @param color
 	 *            the player that owns this piece
 	 *****************************************************************/
 	protected Knight(final Player color) {
 		super(color);
 	}
-	
+
 
 	/*****************************************************************
 	 * Returns the type of ChessPiece it is.
-	 * 
+	 *
 	 * @return the String value of the name of the ChessPiece
 	 *****************************************************************/
 	@Override
@@ -29,10 +29,10 @@ public final class Knight extends ChessPiece {
 		return "Knight";
 	}
 
-	
+
 	/*****************************************************************
 	 * Checks if a move is valid for the chess piece.
-	 * 
+	 *
 	 * @param move
 	 *            the move to check for validity
 	 * @param board
@@ -42,12 +42,12 @@ public final class Knight extends ChessPiece {
 	 * @return true if the move is valid, else false
 	 *****************************************************************/
 	@Override
-	public boolean isValidMove(final Move move, 
+	public boolean isValidMove(final Move move,
 			final IChessBoard board, final IChessModel model) {
 		if (!super.isValidMove(move, board, model)) {
 			return false;
 		}
-			
+
 		// Knight can jump over another chess piece or pieces
 		// Can move two squares either forward, backward, left, or right
 		// and then left or right one square.
@@ -60,18 +60,19 @@ public final class Knight extends ChessPiece {
 		}
 
 		if (Math.abs(move.getFromColumn() - move.getToColumn()) == 2) {
-			if (Math.abs(move.getFromRow() - move.getToRow()) == 1) {
+		   if (Math.abs(move.getFromRow() - move.getToRow()) == 1) {
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
-	
-	
+
+
 	/*****************************************************************
 	 * Set the current piece to reflect it being moved.
-	 * 
+	 *
+	 *
 	 * @param moved
 	 *            a boolean stating if the piece was moved
 	 *****************************************************************/
@@ -79,12 +80,12 @@ public final class Knight extends ChessPiece {
 	public void setMoved(final boolean moved) {
 		super.setMoved(moved);
 	}
-	
-	
+
+
 	/*****************************************************************
-	 * Returns a boolean that is true or false depending on if the 
+	 * Returns a boolean that is true or false depending on if the
 	 * piece has moved yet.
-	 * 
+	 *
 	 * @return a boolean stating if the piece has moved yet or not
 	 *****************************************************************/
 	@Override
